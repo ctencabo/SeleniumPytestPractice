c2889657898616ca7f74aa68b13bc6e5794ce48b
Christian <encabo.chris@gmail.com>
2022-05-02 00:01:27 +0800

    added additional page elements

--- page_objects/landing_page/landing_page_actions.py
+++ page_objects/landing_page/landing_page_actions.py
@@ -1,4 +1,5 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
 
 from .landing_page_objects import LandingPageElements
 
@@ -10,7 +11,61 @@ class LandingPageActions:
     # CLICK ACTIONS
 
     def click_sign_in(self):
-        element = self.driver.find_element(By.XPATH, landing_page_elements.nav_bar_sign_in_text_xpath)
+        element = self.driver.find_element(
+            By.XPATH,
+            landing_page_elements.nav_bar_sign_in_text_xpath)
         self.driver.execute_script("arguments[0].click()", element)
 
-    
+    def click_popular_tab(self):
+        element = self.driver.find_element(
+            By.XPATH,
+            landing_page_elements.popular_tab_xpath)
+        self.driver.execute_script("arguments[0].click()", element)
+
+    def click_bestseller_tab(self):
+        element = self.driver.find_element(
+            By.XPATH,
+            landing_page_elements.bestsellters_tab_xpath)
+        self.driver.execute_script("arguments[0].click()", element)
+
+    def click_popular_first(self):
+        element = self.driver.find_element(
+            By.XPATH,
+            landing_page_elements.popular_first_img_xpath)
+        self.driver.execute_script("arguments[0].click()", element)
+
+    def click_bestseller_first(self):
+        element = self.driver.find_element(
+            By.XPATH,
+            landing_page_elements.bestseller_first_img_xpath)
+        self.driver.execute_script("arguments[0].click()", element)
+
+    def click_quick_view_btn(self):
+        element = self.driver.find_element(
+            By.XPATH,
+            landing_page_elements.quickview_btn_xpath)
+        self.driver.execute_script("arguments[0].click()", element)
+
+    def click_add_to_cart_hover_btn(self):
+        element = self.driver.find_element(
+            By.XPATH,
+            landing_page_elements.hover_add_to_cart_btn_xpath)
+        self.driver.execute_script("arguments[0].click()", element)
+
+    # HOVER ACTIONS
+
+    def hover_to_popular_first(self):
+        action = ActionChains(self.driver)
+        element = self.driver.find_element(
+            By.XPATH,
+            landing_page_elements.popular_first_img_xpath)
+        self.driver.execute_script("arguments[0].scrollIntoView()", element)
+        action.move_to_element(element).perform()
+
+    def hover_to_bestseller_first(self):
+        action = ActionChains(self.driver)
+        element = self.driver.find_element(
+            By.XPATH,
+            landing_page_elements.bestseller_first_img_xpath)
+        self.driver.execute_script("arguments[0].scrollIntoView()", element)
+        action.move_to_element(element).perform()
--- page_objects/landing_page/landing_page_objects.py
+++ page_objects/landing_page/landing_page_objects.py
@@ -19,18 +19,24 @@ class LandingPageElements:
     popular_tab_xpath = ".//a[@class='homefeatured']"
     bestsellters_tab_xpath = ".//a[@class='blockbestsellers']"
 
-    hover_price_xpath = "//div[@class='left-block']//div[@class='content_price']"
-    unhovered_price_xpath = "//div[@class='right-block']//div[@class='content_price']"
+    hover_price_xpath = "//div[@class='left-block']" \
+        "//div[@class='content_price']"
+    unhovered_price_xpath = "//div[@class='right-block']" \
+        "//div[@class='content_price']"
 
     # Buttons
     women_btn_xpath = ".//a[@title='Women']"
-    dresses_btn_xpath = ".//ul[@class='sf-menu clearfix menu-content sf-js-enabled sf-arrows']/li/a[@title='Dresses']"
-    tshirt_btn_xpath = ".//ul[@class='sf-menu clearfix menu-content sf-js-enabled sf-arrows']/li/a[@title='T-shirts']"
+    dresses_btn_xpath = ".//ul[@class='sf-menu clearfix menu-content" \
+        " sf-js-enabled sf-arrows']/li/a[@title='Dresses']"
+    tshirt_btn_xpath = ".//ul[@class='sf-menu clearfix menu-content" \
+        " sf-js-enabled sf-arrows']/li/a[@title='T-shirts']"
 
-    quickview_btn_xpath = "//a[@class='quick-view']"
+    quickview_btn_xpath = "//li[contains(@class,'hovered')]" \
+        "//a[@class='quick-view']"
 
-    hover_add_to_cart_btn_xpath = "//a[@title='Add to cart']"
-    hover_more_btn_xpath = "//a[@title='View']"
+    hover_add_to_cart_btn_xpath = "//li[contains(@class,'hovered')]" \
+        "//a[@title='Add to cart']"
+    hover_more_btn_xpath = "//li[contains(@class,'hovered')]//a[@title='View']"
 
     # Search Bars
     search_bar_id = "search_query_top"
@@ -38,4 +44,51 @@ class LandingPageElements:
     # Images
     logo_xpath = ".//img[@class='logo img-responsive']"
     popular_first_img_xpath = "{popular}//img".format(popular=popular_first)
-    bestseller_first_img_xpath = "{bestseller}//img".format(bestseller=bestseller_first)
+    bestseller_first_img_xpath = "{bestseller}//img".format(
+        bestseller=bestseller_first)
+
+
+class QuickViewElements:
+    """Elements found inside the quick view modal"""
+
+    # Texts/Labels
+
+    price_id = "our_price_display"
+    product_name_xpath = "//h1[@itemprop='name']"
+    product_ref_id = "product_reference"
+    product_condition_id = "product_condition"
+    product_description_id = "short_description_content"
+    quantity_label_xpath = "//p[@id='quantity_wanted_p']/label"
+    size_label_xpath = "//label[@class='attribute_label']" \
+        "[contains(text(),'Size')]"
+    color_label_xpath = "//label[@class='attribute_label']" \
+        "[contains(text(),'Color')]"
+
+    # Buttons
+
+    close_btn_xpath = "//a[@title='Close']"
+    minus_btn_xpath = "//a[contains(@class,'product_quantity_down')]"
+    plus_btn_xpath = "//a[contains(@class,'product_quantity_up')]"
+    color_selected_xpath = "//ul[@id='color_to_pick_list']" \
+        "/li[@class='selected']"
+    color_unselected_xpath = "//ul[@id='color_to_pick_list']" \
+        "/li[@class='selected']/following-sibling::li[position()=1]"
+    add_to_cart_btn_xpath = "//button[@name='Submit'][@class='exclusive']"
+    add_to_wishlist_id = "wishlist_button"
+
+    next_btn_id = "view_scroll_right"
+    prev_btn_id = "view_scroll_left"
+
+    # Input Fields
+
+    quantity_input_field_id = "quantity_wanted"
+
+    # Dropdown
+
+    size_dropdown_id = "group_1"
+
+    # Images
+
+    img_preview_id = "bigpic"
+    img_list_id = "thumbs_list_frame"
+
